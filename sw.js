25b3fc23f44bce8bf559a260f4d73ae1ad385113
mossx13 <echofineart@gmail.com>
2026-09-21 15:50:31 -0400

    Fix sw.js one-tap cache (restore real file)

--- sw.js
+++ sw.js
@@ -1,1 +1,50 @@
-placeholder
+const CACHE = "paws-passions-v2";
+const PRECACHE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./app/icon-192.png",
+  "./app/icon-512.png",
+  "./app/apple-touch-icon.png",
+  "./gallery/home-dog.jpg",
+  "./gallery/window-cat.jpg",
+  "./gallery/evening-walk.jpg",
+  "./gallery/overnight-sofa.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  const isHtml = event.request.mode === "navigate" || url.pathname.endsWith(".html") || url.pathname === "/" || url.pathname.endsWith("/");
+  if (isHtml) {
+    event.respondWith(
+      fetch(event.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match(event.request).then((hit) => hit || caches.match("./index.html")))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((hit) => hit || fetch(event.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
